f09e9f9d3e41224f7bd78f8e52cb581cf6d5aaa8
Joca02 <jocaabramovic@gmail.com>
2024-02-27 00:12:17 +0100

    init

---
+++ Stack/Stack/_1021.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+    internal class _1021
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(RemoveOuterParentheses("(()())(())(()(()))"));
+        }
+
+        static string RemoveOuterParentheses(string s)
+        {
+            string res = "";
+            Stack<char> stack = new Stack<char>();
+            foreach(char c in s)
+            {
+                if (c=='(')
+                {
+                    if (stack.Count > 0) res+=c;
+                    stack.Push(c);
+                }
+                    
+                else if (c==')')
+                {
+                    stack.Pop();
+                    if(stack.Count > 0)res+=c;
+                }
+
+            }
+
+            return res;
+        }
+    }
+}
---
+++ Stack/Stack/_20.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+    internal class _20
+    {
+        /*static void Main(string[] args)
+        {
+            Console.WriteLine( isValid("({}[[]])"));
+        }*/
+
+
+        static public bool isValid(string s)
+        {
+            Stack<char> stack = new Stack<char>();
+            foreach (char c in s)
+            {
+                if (c=='{'||c=='['||c=='(')
+                    stack.Push(c);
+                else if (c=='}'||c==']'||c==')')
+                {
+                    switch (c)
+                    {
+                        case '}':
+                            if(stack.Count > 0)
+                            {
+                                if (stack.Pop()=='{')
+                                    continue;
+                                else return false;
+                            }
+                            else return false;
+                           
+                        case ']':
+                            if (stack.Count > 0)
+                            {
+                                if (stack.Pop()=='[')
+                                    continue;
+                                else return false;
+                            }
+                            else return false;
+                        case ')':
+                            if (stack.Count > 0)
+                            {
+                                if (stack.Pop()=='(')
+                                    continue;
+                                else return false;
+                            }
+                            else return false;
+                    }
+
+                }
+                else return false;
+
+            }
+            if (stack.Count == 0) return true;
+            return false; //ako nema ni1 char
+        }
+       
+    }
+}

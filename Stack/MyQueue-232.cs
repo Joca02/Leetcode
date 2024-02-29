18cfea50e9691260eb6af0e930fc325901e3f4a4
Joca02 <jocaabramovic@gmail.com>
2024-02-29 23:41:04 +0100

    komitqueueue

--- Stack/MyQueue-232.cs
+++ Stack/MyQueue-232.cs
@@ -8,5 +8,48 @@ namespace Stack
 {
     internal class MyQueue_232
     {
+   
+        Stack<int>stack = new Stack<int>();
+        Stack<int> queue = new Stack<int>();
+
+        public void GenerateQueue()
+        {
+            queue.Clear();
+            foreach (var item in stack)
+            {
+                queue.Push(item);
+            }
+        }
+
+        public void RefreshStack()
+        {
+            stack.Clear();
+            foreach(var item in queue)
+                stack.Push(item);
+        }
+        public void Push(int x)
+        {
+            stack.Push(x);
+            GenerateQueue();
+
+        }
+
+        public int Pop()
+        {
+            var val=queue.Peek();
+            queue.Pop();
+            RefreshStack();
+            return val;
+        }
+
+        public int Peek()
+        {
+            return queue.Peek();
+        }
+
+        public bool Empty()
+        {
+            return queue.Count == 0;
+        }
     }
 }

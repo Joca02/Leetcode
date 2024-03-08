c6cc8860ae8d55855733f82c7dec85c46cfa9dbb
Joca02 <jocaabramovic@gmail.com>
2024-03-09 00:26:10 +0100

    linq200

---
+++ Array/SingleNumber-136.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    internal class SingleNumber_136
+    {
+        public static int SingleNumber(int[] nums)
+        {
+            return nums.GroupBy(x => x).Where(group => group.Count()==1).Select(group => group.Key).First();
+        }
+    }
+}

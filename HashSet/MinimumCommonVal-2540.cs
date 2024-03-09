413b374a267e06a8b6e2b73b26b10bb7c69b69e8
Joca02 <jocaabramovic@gmail.com>
2024-03-09 21:28:49 +0100

    hes je biznis

---
+++ HashSet/MinimumCommonVal-2540.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Heaps
+{
+    internal class Program
+    {
+
+        public static  int GetCommon(int[] nums1, int[] nums2)
+        {
+            var s = nums1.Intersect(nums2);
+            return s.Count()>0?s.Min():-1;
+            
+        }
+    }
+}
---
+++ HashSet/SubsequenceWithLargestSum-2099.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Heap
+{
+    internal class SubsequenceWithLargestSum_2099
+    {
+        public static int[] MaxSubsequence(int[] nums, int k)
+        {
+            var dict=new SortedDictionary<int,int>();
+            var list=new List<int?>(nums.Select(x=>(int?)x));
+            for(int i=0;i<k;i++)
+            {
+                int max =(int) list.Max(x=>x);
+                int index = list.IndexOf(max);
+                list[index]=null;
+                dict.Add(index,max);
+                
+            }
+
+            int [] result = new int[k];
+            int j = 0;
+            foreach(var num in dict.Values)
+            {
+                result[j++]=num;
+            }
+            return result;
+        }
+    }
+}
